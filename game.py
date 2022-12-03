ec0788184749fe3ccb9c22b71f36b7da6a8cf1f0
cux-maks <volcano020716@gmail.com>
2022-12-03 23:56:20 +0900

    20221203

--- game.py
+++ game.py
@@ -1,10 +1,18 @@
 import msvcrt
 import random
 import time
+import copy
+import os
 
 grid = [["ㆍ" for _ in range(4)] for _ in range(4)]
 
-def Create_Goal() -> None:
+now_x = 0
+now_y = 0
+
+def Create_Start(x = 0, y = 0) -> None:
+    grid[x][y] = "☆"
+
+def Create_Goal(x = 3, y = 3) -> None:
     grid[3][3] = "G"
 
 def Create_Hole(difficult: int) -> None:
@@ -13,16 +21,44 @@ def Create_Hole(difficult: int) -> None:
     while hole_cnt < hole_num[difficult]:
         x = random.randrange(4)
         y = random.randrange(4)
-        if not grid[x][y] == "H" and not grid[x][y] == "G":
+        if not grid[x][y] == "H" and not grid[x][y] == "G" and not grid[x][y] == "☆":
             grid[x][y] = "H"
             hole_cnt += 1
 
-def find_root() -> bool:
-    can_go_x = [0, 0, 1, -1]
-    can_go_y = [-1, 1, 0, 0]
+def Delete_Hole() -> None:
+    for x in range(4):
+        for y in range(4):
+            if grid[x][y] == "H": grid[x][y] = "ㆍ"
 
+def find_root() -> bool:
+    visited = []
+    visited.append((0, 0))
+
+    grid_test = copy.deepcopy(grid)
+
+    while not len(visited) == 0:
+        here = visited.pop()
+        (x, y) = here
+        if grid_test[x][y] == "G": return True
+        else:
+            grid_test[x][y] = "☆"
+            if (x >= 0 and x < 4 and y - 1 >= 0 and y - 1 < 4) and (grid_test[x][y - 1] == "ㆍ" or grid_test[x][y - 1] == "G"): visited.append((x, y - 1))
+            if (x - 1 >= 0 and x - 1 < 4 and y >= 0 and y < 4) and (grid_test[x - 1][y] == "ㆍ" or grid_test[x - 1][y] == "G"): visited.append((x - 1, y))
+            if (x >= 0 and x < 4 and y + 1 >= 0 and y + 1 < 4) and (grid_test[x][y + 1] == "ㆍ" or grid_test[x][y + 1] == "G"): visited.append((x, y + 1))
+            if (x + 1 >= 0 and x + 1 < 4 and y >= 0 and y < 4) and (grid_test[x + 1][y] == "ㆍ" or grid_test[x + 1][y] == "G"): visited.append((x + 1, y))
+
+    return False
+
+n = 1
+
+Create_Start()
 Create_Goal()
-Create_Hole(1)
+
+while True:
+    Delete_Hole()
+    Create_Hole(n)
+    if find_root(): break
+
 for x in grid:
     print(x)
 
@@ -38,7 +74,31 @@ def getKey_random() -> str:
     retValue = ["up", "down", "left", "right"]
     return retValue[random.randrange(0, 4)]
 
+def move(direction: str) -> None:
+    global now_y
+    global now_x
+    if direction == "up":
+        if now_y >= 1: now_y -= 1
+    elif direction == "down":
+        if now_y < 3: now_y += 1
+    elif direction == "right":
+        if now_x < 3: now_x += 1
+    elif direction == "left":
+        if now_x >= 1: now_x -= 1
+    elif direction == None: pass
+    change_position()
 
+def change_position() -> None:
+    for x in range(4):
+        for y in range(4):
+            if grid[x][y] == "☆": grid[x][y] = "ㆍ"
+    grid[now_y][now_x] = "☆"
+
+def print_grid() -> None:
+    os.system("cls")
+    for x in grid:
+        print(x)
 
 while True:
-    pass
+    move(getKey())
+    print_grid()
